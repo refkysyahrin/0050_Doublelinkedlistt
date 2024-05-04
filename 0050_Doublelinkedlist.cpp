65f7b9571a13772613360174a5e78e5fe2dba55a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-04 10:37:41 +0700

    newNode->next = current; newNode->prev = previous;

--- 0050_Doublelinkedlist.cpp
+++ 0050_Doublelinkedlist.cpp
@@ -50,4 +50,14 @@ void addNode()
 			current = current->next;
 		}
 
-		
+		newNode->next = current;
+		newNode->prev = previous;
+
+		if (current != NULL)
+		{
+			current->prev = newNode;
+		}
+		if (previous != NULL)
+		{
+			previous->next = newNode;
+		}
